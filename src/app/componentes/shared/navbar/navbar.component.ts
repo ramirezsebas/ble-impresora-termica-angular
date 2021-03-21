ff34fa4155a22098a03da5c08753cb9108e35b42
Sebastian Ramirez <ramirezmatias946@gmail.com>
2021-03-21 00:10:58 -0300

    Modificamos la barra de navegacion eliminando un elemento..

--- src/app/componentes/shared/navbar/navbar.component.ts
+++ src/app/componentes/shared/navbar/navbar.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { PrinterService } from 'src/app/services/printer.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private _printerService: PrinterService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
